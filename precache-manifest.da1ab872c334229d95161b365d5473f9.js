21017c0d381d9c9dba6ba431451e288f02844957
Abdiwahab Ahmed <abdiwahabahmed@Abdiwahabs-MacBook-Air.local>
2020-04-17 23:45:22 -0400

    Updates

---
+++ precache-manifest.da1ab872c334229d95161b365d5473f9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64f1f3a945bfb1bebee15230ecdba685",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "3e251454d6385e028515",
+    "url": "/react-portfolio/static/css/main.7b685dea.chunk.css"
+  },
+  {
+    "revision": "5c3c3db274e6df9e2dff",
+    "url": "/react-portfolio/static/js/2.6d6173c6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.6d6173c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e251454d6385e028515",
+    "url": "/react-portfolio/static/js/main.edb96576.chunk.js"
+  },
+  {
+    "revision": "26950ce1f34a1b315732",
+    "url": "/react-portfolio/static/js/runtime-main.e44c21d0.js"
+  },
+  {
+    "revision": "ffeb2184cc661d5d96e67cc9422a5251",
+    "url": "/react-portfolio/static/media/71287.ffeb2184.jpg"
+  },
+  {
+    "revision": "5a664f35a07fff73f4d3d6937ee994d3",
+    "url": "/react-portfolio/static/media/burger.5a664f35.png"
+  },
+  {
+    "revision": "d8a1229e3a62750dcc40f9b9d48f007d",
+    "url": "/react-portfolio/static/media/chat.d8a1229e.png"
+  },
+  {
+    "revision": "edbc895cf1a7f5e351efbd2146dd6dbc",
+    "url": "/react-portfolio/static/media/door.edbc895c.jpg"
+  },
+  {
+    "revision": "d19962b74c6f3e3beb7b9d2ee817b202",
+    "url": "/react-portfolio/static/media/me.d19962b7.png"
+  },
+  {
+    "revision": "9fa24db0bf0d3a780220199e5e82847c",
+    "url": "/react-portfolio/static/media/people.9fa24db0.png"
+  },
+  {
+    "revision": "8611dbf8a595fda54e4eea8f39fab7e1",
+    "url": "/react-portfolio/static/media/reading.8611dbf8.jpg"
+  },
+  {
+    "revision": "e8cde95b5b1edcfce4328ec8db519214",
+    "url": "/react-portfolio/static/media/resume.e8cde95b.pdf"
+  }
+]);
